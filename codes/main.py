4f7eba8cf1eef292dae22209c09a663e8444f89f
robredomikel <robredomikel@gmail.com>
2024-02-15 12:58:53 +0200

    Implements main part of the ts_modelling script

--- codes/main.py
+++ codes/main.py
@@ -4,9 +4,14 @@ This project executes a series of TSA techniques as well as ML models
 NOTE: The data collection and preprocessing has been already performed.
 """
 
+import os
 from ml_modelling import ml_models
 from ts_modelling import ts_models
+from commons import DATA_PATH
 
 
 def main():
 
+    results_path = os.path.join(DATA_PATH, 'results')
+    if not os.path.exists(results_path):
+        os.mkdir(results_path)
--- codes/ts_modelling.py
+++ codes/ts_modelling.py
@@ -9,6 +9,18 @@ import numpy as np
 from commons import DATA_PATH
 
 
+def arimax_model(df_path, project_name, periodicity):
+    """
+    Performs the modelling of the ARIMAX model.
+
+    :param df_path: Path of the existing csv file with project data
+    :param project_name: Name of the project
+    :param periodicity: Periodicity level between observations
+    :return model assessment metrics
+    """
+    
+
+
 def ts_models():
     """
     Executes the tsa process
@@ -31,5 +43,19 @@ def ts_models():
     for i in range(len(biweekly_files)):
 
         project = biweekly_files[i][:-4]
-        monthly_results_path
+        arimax_results_path = os.path.join(DATA_PATH, "results", "arimax")
+        if not os.path.exists(arimax_results_path):
+            os.mkdir(arimax_results_path)
+            os.mkdir(os.path.join(arimax_results_path, "monthly_results"))
+            os.mkdir(os.path.join(arimax_results_path, "biweekly_results"))
 
+        monthly_results_path = os.path.join(arimax_results_path, "monthly_results", project)
+        biweekly_results_path = os.path.join(arimax_results_path, "biweekly_results", project)
+
+        if os.path.exists(monthly_results_path) and os.path.exists(biweekly_results_path):
+            continue
+
+        # biweekly_statistics = arimax_model()
+        # Need to at results in the correct format to the DF
+
+        monthly_statistics = arimax_model()
