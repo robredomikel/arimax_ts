0ba10c128bdebfe9c24c43530f4226b30b9b00ba
robredomikel <robredomikel@gmail.com>
2025-01-14 12:13:01 +0100

    Updates gitignore file

---
+++ codes/revision_demo.py
@@ -0,0 +1,10 @@
+"""
+Performs the predictions with the selected models ARIMAX and SARIMAX with a 70/30 split for survey demo to practitioners
+"""
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
